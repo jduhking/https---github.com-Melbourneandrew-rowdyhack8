598eb893e973854464f7a7cabcb169130513b135
jduhking <jamesbodebiyi@gmail.com>
2023-03-25 17:11:12 -0500

    Finished adding routing support

--- App.js
+++ App.js
@@ -2,18 +2,23 @@ import * as React from 'react';
 import { StyleSheet, Text, SafeAreaView,} from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 import HomePage from './screens/HomePage'
 import RecordingPage from './screens/RecordingPage'
 import ChatPage from './screens/ChatPage'
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
-    <SafeAreaView>
-     <HomePage/>
-    </SafeAreaView>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Recording" component={RecordingPage} />
+        <Stack.Screen name="Chat" component={ChatPage} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import {View, Text, TextInput} from 'react-native'
+import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native'
+import { Formik } from 'formik'
 
-const HomePage = () => {
+
+const HomePage = ({navigation}) => {
 
 
 
@@ -11,19 +13,57 @@ const HomePage = () => {
             <View className="text-center my-8">
             <Text className="text-4xl">Knowlater</Text>
             </View>
+            <Formik
+
+            initialValues={{username: '', password: ''}}
+            onSubmit={(values) => {
+
+                console.log(values)
+                navigation.navigate('Recording')
+            
+            }
+            }
+            
+            
+            
+            >
+                {({ handleChange, handleBlur, handleSubmit, values}) => (
+                    <View>
+                        {/* Login */}
             <View className="flex flex-row mt-6 h-12">
+                
                 <Text className="text-2xl">
-                    Login
+                    Username
                 </Text>
-                <TextInput className="w-3/5 rounded-3xl h-full border-4 border-black ml-12 pl-4 pr-4 text-xl"/>
+                <TextInput className="w-3/5 rounded-3xl h-full border-4 border-black ml-3 pl-4 pr-4 text-xl"
+                placeholder="Username"
+                onChangeText={handleChange('username')}
+                onBlur={handleBlur('username')}
+                value={values.username}
+                />
             </View>
+                        {/* Password */}
             <View className="flex flex-row mt-4 h-12">
                 <Text className="text-2xl">
                     Password
                 </Text>
-                <TextInput className="w-3/5 rounded-3xl border-4 border-black ml-2 pl-4 pr-4 text-xl" />
+                <TextInput className="w-3/5 rounded-3xl border-4 border-black ml-4 pl-4 pr-4 text-xl" 
+                placeholder="Password"
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+                />
             </View>
+       
+            
+                <Button onPress={handleSubmit} title="Login" />
+
             </View>
+            )}
+            </Formik>
+                
+            </View>
+           
         </View>
     )
 }
